19530dae190d7749e4e88a4876bdab968c987ab9
dzbhhz <dzbhhz@gmail.com>
2015-10-08 13:16:19 -0400

    Update number_in_square.py

--- number_in_square.py
+++ number_in_square.py
@@ -37,21 +37,18 @@ lat_is = np.linspace(latsize[0],latsize[1],1000)
 LON,LAT,NUMBER=[],[],[]
 for j in range(len(lon_i)):
     for i in range(len(lat_i)):
-        LON.append(round(lon_i[j],1))
-        NUMBER.append(number[j][i])
-for i in range(len(lon_i)):
-    for j in range(len(lat_i)):
-        LAT.append(round(lat_i[j],1))                    #use for griddata
-number_i = griddata(np.array(LON),np.array(LAT),np.array(NUMBER),lon_is,lat_is)
-depth_i = griddata(np.array(lon),np.array(lat),np.array(data['MEAN_DEPTH']),lon_is,lat_is)
+        LON.append(lon_i[j])
+        LAT.append(lat_i[i])
+        NUMBER.append(number[j][i])                    #use for griddata
+number_i = griddata(np.array(LON),np.array(LAT),np.array(NUMBER),lon_is,lat_is,interp='linear')
+depth_i = griddata(np.array(lon),np.array(lat),np.array(data['MEAN_DEPTH']),lon_is,lat_is,interp='linear')
 
 fig=plt.figure(figsize=(12,10))
 ax=fig.add_subplot(111)
 draw_basemap(fig,ax,lonsize,latsize)
-CS1=plt.contour(lon_is, lat_is,depth_i,np.arange(100,102,100),colors='r',linewidths=4,linestyles=':')   #plot 100m depth
-CS = plt.contourf(lon_is, lat_is, number_i, np.arange(0,200,5), cmap=plt.cm.rainbow,
-                  vmax=abs(number_i).max(), vmin=-abs(number_i).max())
-cbar=plt.colorbar(CS)
+#CS1=plt.contour(lon_is, lat_is,depth_i,np.arange(100,102,100),colors='r',linewidths=4,linestyles=':')   #plot 100m depth
+CS = plt.contourf(lon_is, lat_is, number_i, np.arange(1,101,1), cmap=plt.cm.rainbow,vmax=101, vmin=1)
+cbar=plt.colorbar(CS,ticks=[1,20,40,60,80,100])
 cbar.ax.tick_params(labelsize=20)
 #x=(min(lon)+176*one_minute,min(lon)+430*one_minute,min(lon)+236*one_minute,min(lon)+288*one_minute)
 #y=(min(lat)+254*one_minute,min(lat)+300*one_minute,min(lat)+252*one_minute,min(lat)+406*one_minute)
